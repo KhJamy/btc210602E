9332c2545cdb9bab4c58491f22e5d3284a210f30
Khandaker Jamy <rownakhasan@yahoo.com>
2021-12-06 20:23:23 -0500

    second commit

---
+++ MavenProject_BTC2106/src/main/java/Db/ReadFile.java
@@ -0,0 +1,19 @@
+package Db;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadFile {
+	
+	public Properties readProfile(String path) throws IOException {
+		
+		FileInputStream f = new FileInputStream(path);
+		Properties prop = new Properties();
+		prop.load(f);
+		return prop;
+		
+	}
+
+}
---
+++ MavenProject_BTC2106/src/test/java/com/test/BaseTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.junit.Before;
+
+import Db.ReadFile;
+
+public class BaseTest {
+	
+	ReadFile f = new ReadFile();
+	
+	
+	
+
+}
